cc8fb8a387f30a7e8b7dfa75992763c587616a4d
PunitCodeX <punitdhankhar21@gmail.com>
2025-11-13 20:18:02 +0530

    Day1.py

---
+++ Python/Day1.py
@@ -0,0 +1,108 @@
+#Made By Punit (Saitm)
+
+=================== x x x x x x x x ===================
+
+print("Hello")
+
+""" This is Comment. """
+
+#This is Comment
+
+"""This is Comment"""
+
+""" This """
+
+""" 
+
+This
+... is
+... comment
+
+"""
+
+x=10
+print(x)
+
+=================== x x x x x x x x ===================
+a=10
+b=90
+c=a+b
+print(c)
+
+=================== x x x x x x x x ===================
+
+x=5
+x=float(45)
+print(x)
+
+=================== x x x x x x x x ===================
+
+x=str(1)
+print(x)
+
+=================== x x x x x x x x ===================
+
+x=5
+print(type(x))
+
+=================== x x x x x x x x ===================
+
+x="a"
+print(type(x))
+
+=================== x x x x x x x x ===================
+
+print("hello",end=" ")
+
+print("hello",end="2")
+
+print("hello",end="")
+
+=================== x x x x x x x x ===================
+
+print(22/7)
+
+print(22//7)
+
+print(22%7)
+
+=================== x x x x x x x x ===================
+
+a=18
+print("my age is",a,"years old")
+
+=================== x x x x x x x x ===================
+
+x=y=z=10
+print(x)
+
+print(z)
+
+=================== x x x x x x x x ===================
+
+print(a[-2:-5])
+
+print(a[-2:-5:-1])
+
+=================== x x x x x x x x ===================
+
+a="abcd"
+print(a.upper())
+
+z="ABCD"
+print(z.replace("B","K"))
+
+y="ABC"
+print(y.replace("A","K"))
+
+=================== x x x x x x x x ===================
+
+print(a+" "+z)
+
+print(a+"_"+z)
+
+=================== x x x x x x x x ===================
+
+
+#Made By Punit 
+#Saitm
